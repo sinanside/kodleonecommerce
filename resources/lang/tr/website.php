e95dc49409cc963e3fbddb7243a60b7f1a9c7f7e
sinanside <mackaengin@gmail.com>
2019-12-08 18:40:34 +0200

    first comit

---
+++ resources/lang/tr/website.php
@@ -0,0 +1,730 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+		
+	'bannerLabel1'=>'Ücretsiz Kargo',
+		
+	'bannerLabel1Text'=>'50TL ve üzeri alışverişlerinizde',
+		
+	'bannerLabel2'=>'Para İadesi',
+		
+	'bannerLabel2Text'=>'30 gün koşulsuz para iadesi',
+		
+	'bannerLabel3'=>'24/7 Destek',
+		
+	'hotline'=>'Tel',
+	
+	'bannerLabel4'=>'Güvenli Ödeme',
+		
+	'bannerLabel4Text'=>'Güvenli ödeme sistemi',
+		
+	'TopSales'=>'En Çok Satanlar',
+		
+	'Special'=>'Özel Ürünler',
+		
+	'MostLiked'=>'En Beğenilen Ürünler',
+		
+	'New'=>'Yeni',
+		
+	'Likes'=>'Beğeniler',
+		
+	'Welcome to our Store'=>'Mağazamıza hoş geldiniz.',
+		
+	'Orders'=>'Siparişler',
+		
+	'All Categories'=>'Tüm Kategoriler',
+		
+	'Search entire store here'=>'Aradığınız ürünü yazın',
+		
+	'Profile'=>'Profil',
+		
+	'Logout'=>'Çıkış',
+		
+	'Login/Register'=>'Giriş/Kayıt',
+		
+	'item(s)'=>'ürün(ler)',
+		
+	'Quantity'=>'Adet',
+		
+	'Total Items'=>'Toplam Ürün',
+		
+	'Total Price'=>'Toplam Fiyat',
+		
+	'View Cart'=>'Sepetim',
+		
+	'Checkout'=>'Ödeme',
+		
+	'Menu'=>'Menu',	
+		
+	'My Cart'=>'Sepetim',
+		
+	'footer text'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel nulla eros. Sed dui magna, faucibus et enim vel, auctor dapibus ante. Nullam in sem ultrices, efficitur arcu quis, fermentum mi.',
+		
+	'Helpful Links'=>'Yardımcı Linkler',
+		
+	'Terms'=>'Şartlar',
+		
+	'Privacy'=>'Gizlilik',
+		
+	'Discount'=>'İndirim',
+		
+	'About Us'=>'Hakkımızda',
+		
+	'Contact Us'=>'Bize Ulaşın',
+		
+	'Terms & Condtions'=>'Şartlar & Koşullar',
+		
+	'Wishlist'=>'İstek Listesi',
+		
+	'Shopping Cart'=>'Alışveriş Sepetim',
+		
+	'Privacy Policy'=>'Gizlilik Politikası',
+		
+	'Phone'=>'Telefon',
+		
+	'Email'=>'Email',
+		
+	'Copy Rights'=>'2019 Dizaynschool',
+		
+	'From our News'=>'From our News',
+		
+	'View All News'=>'View All News',
+		
+	'Readmore'=>'Readmore',
+		
+	'Weekly'=>'Weekly',
+		
+	'banner saving text'=>'Saving up to 50% off all online store items this week.',
+		
+	'Shop now'=>'Shop now',
+		
+	'Sale'=>'Sale',
+		
+	'Top Selling of the Week'=>'Haftanın en çok satanları',
+		
+	'View Detail'=>'Ürün Detayı',
+		
+	'Newest Products'=>'Yeni Ürünler',
+		
+	'View All'=>'Tamamını gör',
+	
+	'Special'=>'Özel Ürünler',
+		
+	'items'=>'ürün(ler)',
+	'item'=>'Ürün',
+		
+	'SubTotal'=>'Alt Toplam',
+		
+	'Couponisappliedsuccessfully'=>'Kupon başarıyla uygulandı!',
+		
+	'You are not allowed to use this coupon'=>'You are not allowed to use this coupon.',
+		
+	'This coupon has been reached to its maximum usage limit'=>'This coupon has been reached to its maximum usage limit.',
+		
+	'coupon is used limit'=>'Coupon is used limit',
+		
+	'Coupon amount limit is low than minimum price'=>'Coupon amount limit is low than minimum price.',
+		
+	'Coupon amount limit is exceeded than maximum price'=>'Coupon amount limit is exceeded than maximum price.',
+		
+	'Coupon cannot be applied this product is in sale'=>'Coupon cannot be applied this product is in sale.',
+		
+	'Coupon amount is greater than total price'=>'Coupon amount is greater than total price.',
+		
+	'Coupon amount is greater than product price'=>'Coupon amount is greater than product price.',
+		
+	'Coupon does not exist'=>'Coupon does not exist or invalid coupon code.',
+		
+	'Please enter a valid coupon code'=>'Please enter a valid coupon code',
+		
+	'Coupon is already applied'=>'Coupon is already applied',
+		
+	'Cart item has been deleted successfully'=>'Cart item has been deleted successfully!',
+		
+	'Coupon has been removed successfully'=>'Coupon has been removed successfully!',
+		
+	'Cart has been updated successfully'=>'Cart has been updated successfully!',
+		
+	'Coupon can not be apllied to empty cart'=>'Coupon can not be apllied to empty cart.',
+		
+	'cartEmptyText'=>'You have no items in your shopping cart.<br>Click <a href="shop">here</a> to continue shopping.',
+		
+	'Home'=>'Anasayfa',
+		
+	'Shopping cart'=>'Shopping cart',
+		
+	'Poducts'=>'Ürünler',
+		
+	'Detail'=>'Detail',
+		
+	'Quantity'=>'Adet',
+		
+	'Unit Price'=>'Unit Price',
+		
+	'Item Total'=>'Item Total',
+		
+	'Special'=>'Özel Ürünler',
+		
+	'Coupon Code'=>'Kupon kodu',
+		
+	'Checkout'=>'Ödeme',
+		
+	'Update Cart'=>'Update Cart',
+		
+	'Back To Shopping'=>'Back To Shopping',
+		
+	'Total'=>'Toplam',
+		
+	'Discount(Coupon)'=>'İndirim (Kupon)',
+		
+	'Order Summary'=>'Alışveriş Özeti',
+		
+	'Subtotal'=>'Alt-toplam',
+		
+	'Apply'=>'Uygula',
+		
+	'Please enter your first name'=>'Lütfen adınızı giriniz.',
+		
+	'Please enter your last name'=>'Lütfen soyadınızı giriniz.',
+		
+	'Login'=>'Giriş',
+		
+	'Create An Account'=>'Yeni Hesap Oluştur',
+		
+	'Personal Information'=>'Kişisel Bilgilerim',
+		
+	'Error'=>'Hata',
+		
+	'Success'=>'Başarılı',
+		
+	'First Name'=>'Adınız',
+		
+	'Last Name'=>'Soyadınız',
+		
+	'Email Adrress'=>'Mail adresi',
+		
+	'Please enter your valid email address'=>'Lütfen geçerli bir mail adresi girin.',
+		
+	'Gender'=>'Cinsiyet',
+		
+	'Please select your gender'=>'Cinsiyetinizi giriniz.',
+		
+	'Picture'=>'Resim',
+		
+	'Password'=>'Şifre',
+		
+	'Please enter your password'=>'Şifrenizi girin.',
+		
+	'Confirm Password'=>'Confirm Password',
+		
+	'Please re-enter your password'=>'Please re-enter your password.',
+		
+	'Password does not match the confirm password'=>'Password does not match the confirm password.',
+		
+	'Creating an account means you are okay with our'=>'Creating an account means you are okay with our',
+		
+	'Terms and Services'=>'Terms and Services',
+		
+	'Privacy Policy'=>'Gizlilik Politikası',
+		
+	'Refund Policy'=>'İade Şartları',
+		
+	'Please accept our terms and conditions'=>'Please accept our terms and conditions',
+		
+	'Sign Up'=>'Kayıt ol',
+		
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		
+	'Duis at nisl luctus, malesuada diam non, mattis odio'=>'Duis at nisl luctus, malesuada diam non, mattis odio.',
+		
+	'Fusce porta neque at enim consequat, in vulputate tellus faucibus'=>'Fusce porta neque at enim consequat, in vulputate tellus faucibus.',
+		
+	'Pellentesque suscipit tortor id dui accumsan varius'=>'Pellentesque suscipit tortor id dui accumsan varius.',
+		
+	'Sed interdum purus imperdiet tortor imperdiet, et ultricies leo gravida'=>'Sed interdum purus imperdiet tortor imperdiet, et ultricies leo gravida.',
+		
+	'Aliquam pharetra urna vel nulla egestas, non laoreet mauris mollis'=>'Aliquam pharetra urna vel nulla egestas, non laoreet mauris mollis.',
+		
+	'Integer sed velit sit amet quam pharetra ullamcorper'=>'Integer sed velit sit amet quam pharetra ullamcorper.',
+		
+	'Proin eget nulla accumsan, finibus lacus aliquam, tincidunt turpis'=>'Proin eget nulla accumsan, finibus lacus aliquam, tincidunt turpis.',
+		
+	'Nam at orci tempor, mollis mi ornare, accumsan risus'=>'Nam at orci tempor, mollis mi ornare, accumsan risus.',
+		
+	'Cras vel ante vel augue convallis posuere'=>'Cras vel ante vel augue convallis posuere.',
+		
+	'Ut quis dolor accumsan, viverra neque nec, blandit leo'=>'Ut quis dolor accumsan, viverra neque nec, blandit leo.',
+		
+	'Checkout'=>'Ödeme',
+		
+	'Shopping cart'=>'Shopping cart',
+		
+	'Billing Address'=>'Fatura Adresi',
+		
+	
+		
+	'Payment Methods'=>'Ödeme Seçenekleri',
+		
+	'Order Detail'=>'Sipariş Detayı',
+		
+	'Company'=>'Şirket',
+		
+	'Address'=>'Adres',
+		
+	'Country'=>'Ülke',
+		
+	'State'=>'State',
+		
+	'City'=>'Şehir',
+		
+	'Zip/Postal Code'=>'Posta Kodu',
+		
+	'Continue'=>'Devam et',
+
+	'Please enter your company name'=>'Şirket isminizi giriniz.',
+		
+	'Please enter your address'=>'Please enter your address.',
+		
+	'Please select your country'=>'Ülkenizi seçin.',
+		
+	'Please select your state'=>'Please select your state.',
+		
+	'Please enter your city'=>'Please enter your city.',
+		
+	'Please enter your Zip/Postal Code'=>'Please enter your Zip/Postal Code.',
+		
+	'Update'=>'Güncelle',
+		
+	'Add Address'=>'Add Address',
+		
+	'Your address has been deteled successfully'=>'Your address has been deteled successfully!',
+		
+	'Your address has been chnaged successfully'=>'Your address has been chnaged successfully!',
+		
+	'Default'=>'SEÇİLMİŞ',
+		
+	'Address Info'=>'Adres Bilgisi',
+		
+	'Action'=>'İşlem',
+		
+	'Your address has been updated successfully'=>'Your address has been updated successfully!',
+		
+	'Same shipping and billing address'=>'Same shipping and billing address.',
+		
+	'Select Country'=>'Ülke seçin',
+		
+	'Select State'=>'Select State',
+		
+	'Other'=>'Other',
+		
+	'Shipping Methods'=>'Kargo Seçenekleri',
+		
+	'Confirm Order'=>'Confirm Order',
+		
+	'Order Now'=>'Order Now',
+		
+	'Please select a prefered shipping method to use on this order'=>'Lütfen aşağıdan bir ödeme yöntemi seçin.',
+		
+	'My Orders'=>'Sipariş Listesi',
+		
+	'No order is placed yet'=>'Henüz siparişiniz bulunmamaktadır.',
+		
+	'Order ID'=>'Order ID',
+		
+	'Quantity'=>'Adet',
+		
+	'Order Date'=>'Order Date',
+		
+	'Price'=>'Fiyat Aralığı',
+		
+	'View Order'=>'Siparişi Görüntüle',
+		
+	'Status'=>'Status',
+		
+	'Order information'=>'Sipariş Detayı',
+		
+	'Please select a prefered shipping method to use on this order'=>'Please select a prefered shipping method to use on this order.',
+		
+	'Customer Detail'=>'Customer Detail',
+		
+	'Admin Comments'=>'Admin Comments',
+		
+	'Order Comments'=>'Order Comments',
+		
+	'Comments'=>'Notlar',
+		
+	'Shipping Cost'=>'Kargo Ücreti',
+		
+	'Tax'=>'Tax',
+		
+	'Payment Method'=>'Payment Method',
+		
+	'Shipping Method'=>'Shipping Method',
+		
+	'Payment/Shipping Method'=>'Payment/Shipping Method',
+		
+	'Billing Detail'=>'Billing Detail',
+		
+	'Shipping Detail'=>'Shipping Detail',
+		
+	'Pay'=>'Pay',
+		
+	'Your location does not support this'=>'Your location does not support ',
+		
+	'Please select your shipping method'=>'Please select your shipping method.',
+		
+	'Please select a prefered payment method to use on this order'=>'Please select a prefered payment method to use on this order.',
+		
+	'Please select your payment method'=>'Please select your payment method.',
+		
+	'BrainTree Payment'=>'BrainTree Payment',
+		
+	'Card number'=>'Card number',
+		
+	'Expiration'=>'Expiration',
+		
+	'CVC'=>'CVC',
+		
+	'Payment successful'=>'Payment successful',
+		
+	'Thanks You Your payment has been processed successfully'=>'Thanks You! Your payment has been processed successfully.',
+		
+	'Edit Cart'=>'Edit Cart',
+		
+	'Full Name'=>'İsminiz',
+		
+	'Email'=>'Email',
+		
+	'Message'=>'Mesaj',
+		
+	'Our Address'=>'Our Address',
+		
+	'contact us message'=>'Thank you for contact us. We will contact with you within three business days.',
+		
+	'Subject'=>'Konu',
+		
+	'Send'=>'Gönder',
+	
+	'News Detail'=>'Detay',
+	'Date'=>'Date',
+		
+	'News'=>'Duyurular',
+		
+	'All News'=>'All News',
+		
+	'Sort'=>'Sırala',
+		
+	'Shop'=>'Mağaza',
+		
+	'Newest'=>'En Yeni',
+		
+	'Price: High To Low'=>'Fiyat: Yüksekten Düşüğe',
+		
+	'Price: Low To High'=>'Fiyat: Düşükten Yükseğe',
+		
+	'Top Seller'=>'En Çok Satanlar',
+		
+	'Special Products'=>'Özel Ürünler',
+		
+	'Most Liked'=>'En Çok Beğenilenler',
+		
+	'Display'=>'Göster',
+		
+	'Limit'=>'Sınırla',
+		
+	'No record found'=>'Kayıt bulunamadı!',
+		
+	'All products are loaded.'=>'All products are loaded.',
+		
+	'All news are loaded'=>'All news are loaded.',
+		
+	'Load More'=>'Daha fazla yükle',
+		
+	'Oldest'=>'Oldest',
+		
+	'News Categories'=>'Haber Kategorileri',
+		
+	'A - Z'=>'A - Z',
+		
+	'Z - A'=>'Z - A',
+		
+	'Edit Address'=>'Edit Address',
+		
+	'Add Address'=>'Adres Ekle',
+		
+	'All products are loaded'=>'Tüm ürünler yüklendi.',
+		
+	'Related Products'=>'Benzer Ürünler',
+		
+	'Products Description'=>'Ürün Açıklaması',
+		
+	'Order(s)'=>'Sipariş(ler)',
+		
+	'Login Or Create An Account'=>'Giriş yap veya yeni hesap oluştur',
+		
+	'New Customers'=>'Yeni Müşteriler',
+		
+	'login page text for customer'=>'Mağazamıza kayıt olarak siparişlerinizin takibini daha kolay yapabilir, adres ve kişiler bilgilerinizi kaydederek sonraki alışverişlerinizde kullanabilirsiniz.',
+		
+	'Dont have account'=>'Hesabınız yok mu?',
+		
+	'or Sign in with'=>'veya bunlarla giriş yap',
+		
+	'Login with Facebook'=>'Facebook ile giriş yap',
+		
+	'Login with Google'=>'Google ile giriş yap',
+		
+	'Registered Customers'=>'Kayıtlı Müşteriler',
+		
+	'If you have an account with us, please log in'=>'Eğer hesabınız varsa buradan giriş yapabilirsiniz.',
+		
+	'This field is required'=>'Girilmesi zorunludur.',
+		
+	'Forgot Password'=>'Şifremi Unuttum?',
+		
+	'Please enter your password and should be at least 6 characters long'=>'Please enter your password and should be at least 6 characters long.',
+		
+	'New Password'=>'Yeni Şifre',
+		
+	'Current Password'=>'Şifreniz',
+		
+	'Please enter current password'=>'Please enter current password.',
+		
+	'Please enter your valid phone number'=>'Please enter your valid phone number.',
+		
+	'Phone Number'=>'Telefon',
+		
+	'Please enter your date of birth'=>'Doğum tarihinizi giriniz.',
+		
+	'Date of Birth'=>'Doğum tarihi',
+		
+	'Female'=>'Kadın',
+		
+	'Male'=>'Erkek',
+		
+	'Gender'=>'Cinsiyet',
+		
+	'Ecommerce'=>'E-ticaret',
+		
+	'Featured'=>'Özel',
+		
+	'Filters'=>'Filtreler',
+		
+	'Apply'=>'Uygula',
+		
+	'Reset'=>'İptal',
+		
+	'Subscribe'=>'Subscribe!',
+		
+	'Shipping Address'=>'Teslimat Adresi',
+		
+	'Returned all products'=>'Returned all products.',
+		
+	'Search results empty'=>'Search results empty.',
+		
+	'Categories'=>'Kategoriler',
+		
+	'Product Detail'=>'Product Detail',
+		
+	'Returned all filters successfully'=>'Returned all filters successfully.',
+		
+	'Filter is empty for this category'=>'Filter is empty for this category.',
+		
+	'Select Zone'=>'Select Zone',
+		
+	'select Country'=>'Ülke seçin',
+		
+	'All Products'=>'All Products',
+		
+	'Product is added'=>'Ürün eklendi!',
+		
+	'Special products of the Week'=>'Haftaya Özel Ürünler',
+		
+	'Forgot Password'=>'Şifremi Unuttum',
+		
+	'Please Enter your email to recover your password'=>'Please Enter your email to recover your password.',
+		
+	'Qty'=>'SAYI',
+		
+	'You have no items in your shopping cart'=>'You have no items in your shopping cart.',
+		
+	'Password has been sent to your email address'=>'Password has been sent to your email address.',
+		
+	'Email address does not exist'=>'Email address does not exist.',
+		
+	'Add to Cart'=>'Sepete ekle',
+		
+	'Added'=>'Eklendi!',
+		
+	'Subscribe for Newsletter'=>'Subscribe for Newsletter',
+		
+	'Your email address here'=>'Your email address here...',
+		
+	'Subscribe'=>'Abone ol!',
+		
+	'About Store'=>'Mağaza bilgileri',
+		
+	'Our Services'=>'Hizmetlerimiz',
+		
+	'Information'=>'Bilgilendirme',
+		
+	'Popular Categories'=>'Popular Categories',
+		
+	'Error while placing order'=>'Error while placing order.',
+		
+	'Payment has been processed successfully'=>'Payment has been processed successfully!',
+		
+	'Your order has been placed'=>'Your order has been placed',
+		
+	'Email or password is incorrect'=>'Email or password is incorrect!',
+		
+	'something is wrong'=>'something is wrong!',
+		
+	'Email already exist'=>'Email already exist!',
+		
+		
+	'ApplyCoupon'=>'Kuponu uygula',
+		
+	'proceedToCheckout'=>'Proceed To Checkout',
+		
+	'orderNotes'=>'Sipariş notları',
+	
+	'Previous'=>'Geri',
+	'Next'=>'İleri',
+		
+	'Out of Stock'=>'Stokta yok',
+		
+	'In stock'=>'Stokta var',
+		
+	'Low in Stock'=>'Stoğu azalmış',
+		
+	'Edit'=>'Değiştir',
+		
+	'Remove Item'=>'Kaldır',
+		
+	'per page'=>' / sayfa',
+		
+	'orderNotesandSummary'=>'Sipariş Notu / Özet',
+		
+	'Signup'=>'Kayıt ol',
+		
+	'myProfile'=>'Profilim',
+		
+	'orderID'=>'Order ID #',
+		
+	'orderStatus'=>'Order Status',
+		
+	'Please enter your name'=>'Please enter your name.',
+		
+	'Please enter your message'=>'Please enter your message.',
+		
+	'Prfile has been updated successfully'=>'Prfile has been updated successfully!',
+		
+	'Change Password'=>'Şifreyi Değiştir',
+		
+	'infoPages'=>'Bilgilendirme',
+		
+	'homePages'=>'Home Pages',
+		
+	'homePage1'=>'Home Page 1',
+	'homePage2'=>'Home Page 2',
+	'homePage3'=>'Home Page 3',
+	'homePage4'=>'Home Page 4',
+	'homePage5'=>'Home Page 5',
+		
+	'collection'=>'Collection',
+		
+	'hot'=>'Hot',
+		
+	'new in Stores'=>'New in Stores',
+		
+	'categroies'=>'Categroies',
+		
+	'Call Us Free'=>'Call Us Free',
+		
+	'Featured News'=>'Featured News',
+		
+	'Upload Profile Photo'=>'Profil Resmi Yükle',
+		
+	'Choose...'=>'Choose...',
+		
+	'Showing'=>'Gösterilen 1–',
+		
+	'of'=>'toplam',
+		
+	'results'=>'kayıt.',
+		
+	'Password has been updated successfully'=>'Password has been updated successfully!',
+		
+	'Record not found'=>'Kayıt bulunamadı!',
+	'contact us title'=>'Ecommerce Contact Us',
+	'Follow Us'=>'Sosyalmedya',
+		
+	'Shipping addresses are not added yet'=>'Henüz bir teslimat adresi eklemediniz.',
+		
+	'product is not added to your wish list'=>'Product is not added to your wishlist.',
+		
+	'Search result for'=>'Search result for',
+		
+	'item found'=>'item found.',
+	'cancel'=>'cancel',
+	
+	'Please write notes of your order'=>'Sipariş notu ekleyin',
+	'Coupon Applied'=>'Kupon uygulandı',
+	
+	//new
+	'Welcome'=>'Hoşgeldiniz',
+	'Welcome Guest!'=>'Hoşgeldin Misafir!',
+	'Worldwide Express Plus'=>'Worldwide Express Plus',
+	'Standard'=>'Standard',
+	'Worldwide Expedited'=>'Worldwide Expedited',
+	'Worldwide Express'=>'Worldwide Express',
+	'2nd Day Air A.M.'=>'2nd Day Air A.M.',
+	'Next Day Air Early A.M.'=>'Next Day Air Early A.M.',
+	'Next Day Air Saver'=>'Next Day Air Saver',
+	'3 Day Select'=>'3 Day Select',
+	'Ground'=>'Ground',
+	'2nd Day Air'=>'2nd Day Air',
+	'Next Day Air'=>'Next Day Air',
+	'My Account'=>'Hesabım',
+	'Info Pages'=>'Info Pages',
+	'Dummy Text'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum velit est, et semper massa pulvinar ut. Aliquam sagittis augue at lobortis hendrerit. Vestibulum sit amet posuere quam. Vivamus consectetur tortor eu sollicitudin ullamcorper. Sed dignissim scelerisque enim, sit amet fermentum velit imperdiet eu. Phasellus varius tincidunt nibh, sed ornare felis porttitor quis.',
+	
+	'Product is unliked'=>'Ürün beğenisi kaldırıldı.',
+	'Please login first to like this product'=>'Ürünü beğenmek için üye girişi yapmalısınız.',
+	'Product is liked'=>'Ürün beğenildi.',
+	'Product is disliked'=>'Ürün beğenisi kaldırıldı.',
+	'Password has been updated successfully'=>'Password has been updated successfully.',
+	'Empty Site Name'=>'Empty Site Name',
+	'Instamojo Payment'=>'Instamojo Payment',
+	'External Link'=>'External Link',
+	'Flash Sale'=>'Flaş Satışlar',
+	'cancel order'=>'İptal Sipariş',
+	'cancelled'=>'İptal edildi',
+	'Are you sure you want to cancel this order?'=>'Are you sure you want to cancel this order?',
+	'Flash Sale'=>'Flaş Satışlar',
+	'UP COMMING'=>'ÇOK YAKINDA',
+	'Min Order Limit:'=>'Min Order Limit:',
+	'Return'=>'Geri Dön',
+	'Are you sure you want to return this order?'=>'Siparişi iade etmek istediğinize emin misiniz?',
+	'return order'=>'Sipariş İade et',
+	'Avail free shpping on'=>'Ücretsiz kargo için gerekli tutar',
+	'Coupon does not belongs to this product'=>'Coupon does not belongs to this product.',
+	'You have successfully subscribed.'=>'You have successfully subscribed.',
+	'You have already subscribed.'=>'You have already subscribed.',
+	'Some problem occurred, please try again.'=>'Some problem occurred, please try again.',
+	'Please enter your email address'=>'Please enter your email address.'
+		
+	
+];
